507dc6ea0384c744fa2d9dc6f5c652ee14e5e56b
huynt <huynt57@gmail.com>
2015-12-16 11:56:01 +0700

    crud functions

--- protected/views/documentaryRecieve/index.php
+++ protected/views/documentaryRecieve/index.php
@@ -1,13 +1,81 @@
-<?php
-/* @var $this DocumentaryRecieveController */
+<section class="content">
+    <div class="row">
+        <div class="col-md-12">
 
-$this->breadcrumbs=array(
-	'Documentary Recieve',
-);
-?>
-<h1><?php echo $this->id . '/' . $this->action->id; ?></h1>
+            <div class="box">
+                <div class="box-body">
+                    <?php if (Yii::app()->user->hasFlash('error')): ?>
+                        <div class="alert alert-danger alert-dismissable">
+                            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+                            <h4><i class="icon fa fa-ban"></i> Cảnh báo !</h4>
+                            <?php echo Yii::app()->user->getFlash('error'); ?>
+                        </div>
+                    <?php endif; ?>
+                    <?php if (Yii::app()->user->hasFlash('success')): ?>
+                        <div class="alert alert-success alert-dismissable">
+                            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+                            <h4><i class="icon fa fa-check"></i> Thành công !</h4>
+                            <?php echo Yii::app()->user->getFlash('success'); ?>
+                        </div>
+                    <?php endif; ?>
+                </div>
+                <div class="box-header with-border">
+                    <h3 class="box-title">Danh sách công văn</h3>
+                    <a class="btn btn-primary" style="float: right" href="<?php echo Yii::app()->createUrl('documentary/add') ?>">Thêm</a>
+                </div><!-- /.box-header -->
 
-<p>
-	You may change the content of this page by modifying
-	the file <tt><?php echo __FILE__; ?></tt>.
-</p>
+                <div class="box-body">
+                    <table class="table table-bordered">
+                        <tr>
+                            <th>Mã số</th>
+                            <th>Số công văn</th>
+                            <th>Tóm tắt</th>
+                            <th>Người ký</th>
+                            <th>Từ khóa</th>
+                            <th>Tình trạng</th>
+                            <th>Hành động</th>
+                        </tr>
+                        <?php foreach ($models as $item): ?>
+                            <tr>
+                                <td><?php echo $item->id; ?></td>
+                                <td><?php echo $item->number; ?></td>
+                                <td>
+                                    <?php echo $item->shortcut; ?>
+                                </td>
+                                <td> <?php echo $item->signed_user; ?></td>
+                                <td> <?php echo $item->keywords; ?></td>
+                                <td>
+                                    <?php $status = StringHelper::getStatus($item->status); ?>
+                                    <span class="label label-<?php echo $status['lable'] ?>"><?php echo $status['status'] ?></span>
+                                </td>
+                                <td>                                
+                                    <a href="<?php echo Yii::app()->createUrl('documentary/edit', array('id' => $item->id)) ?>"><span class="label label-primary">Sửa</span></a>
+                                    <a href="<?php echo Yii::app()->createUrl('documentary/delete', array('id' => $item->id)) ?>" onclick="if (!confirm('Bạn có chắc chắn xóa không ?, Hành động này không thể khôi phục')) {
+                                                    return false;
+                                                }"><span class="label label-danger">Xóa</span></a>
+                                </td>
+                            </tr>
+                        <?php endforeach; ?>
+                    </table>
+                </div><!-- /.box-body -->
+                <div class="box-footer clearfix">
+                    <?php
+                    $this->widget('CLinkPager', array(
+                        'pages' => $pages,
+                        //    'maxButtonCount' => Yii::app()->params['btn_cnt'],
+                        'htmlOptions' => array('class' => 'pagination pagination-sm no-margin pull-right',
+                        ),
+                        'header' => '',
+                        'prevPageLabel' => 'Trước',
+                        'nextPageLabel' => 'Sau',
+                        'firstPageLabel' => 'Đầu tiên',
+                        'lastPageLabel' => 'Cuối cùng',
+                        'selectedPageCssClass' => 'active',
+                            )
+                    )
+                    ?>
+                </div>
+            </div><!-- /.box -->
+        </div> 
+    </div> 
+</section>
