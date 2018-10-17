d8ea4660ff03746e94c32b4345652039176d8b04
binhnd <binhnd@socicom.com.vn>
2018-10-17 22:51:49 +0700

    Add missing backups folder

---
+++ backups/db-backup-1539791472-9cd21c6d4c60b5808db3c8bb9820763b.sql
@@ -0,0 +1,205 @@
+DROP TABLE tbl_cases;
+
+CREATE TABLE `tbl_cases` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `date_prosecution` int(11) DEFAULT NULL,
+  `accused` text,
+  `detention_period` text,
+  `investigator` text,
+  `investigation_period` text,
+  `case_name` text,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+
+
+
+
+DROP TABLE tbl_cases_abroad_other;
+
+CREATE TABLE `tbl_cases_abroad_other` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `number` varchar(255) DEFAULT NULL,
+  `shortcut` text,
+  `abstract` text,
+  `signed_user` varchar(255) DEFAULT NULL,
+  `keywords` text,
+  `status` int(11) DEFAULT NULL,
+  `date_recieve` int(11) DEFAULT NULL,
+  `number_doc_finish` varchar(255) DEFAULT NULL,
+  `number_doc_send` varchar(255) DEFAULT NULL,
+  `reciever` varchar(255) DEFAULT NULL,
+  `time_in_doc` int(11) DEFAULT NULL,
+  `country` text,
+  `did_by` text,
+  `obj_name` text,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=67 DEFAULT CHARSET=utf8;
+
+
+
+
+DROP TABLE tbl_document;
+
+CREATE TABLE `tbl_document` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` text,
+  `path` text,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+
+
+
+DROP TABLE tbl_documentary;
+
+CREATE TABLE `tbl_documentary` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `number` varchar(255) DEFAULT NULL,
+  `shortcut` text,
+  `abstract` text,
+  `signed_user` varchar(255) DEFAULT NULL,
+  `keywords` text,
+  `status` int(11) DEFAULT NULL,
+  `date_recieve` int(11) DEFAULT NULL,
+  `number_doc_finish` varchar(255) DEFAULT NULL,
+  `number_doc_send` varchar(255) DEFAULT NULL,
+  `reciever` varchar(255) DEFAULT NULL,
+  `time_in_doc` int(11) DEFAULT NULL,
+  `country` text,
+  `did_by` text,
+  `obj_name` text,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=72 DEFAULT CHARSET=utf8;
+
+
+
+
+DROP TABLE tbl_documentary_abroad;
+
+CREATE TABLE `tbl_documentary_abroad` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `number` varchar(255) DEFAULT NULL,
+  `shortcut` text,
+  `abstract` text,
+  `signed_user` varchar(255) DEFAULT NULL,
+  `keywords` text,
+  `status` int(11) DEFAULT NULL,
+  `date_recieve` int(11) DEFAULT NULL,
+  `number_doc_finish` varchar(255) DEFAULT NULL,
+  `number_doc_send` varchar(255) DEFAULT NULL,
+  `reciever` varchar(255) DEFAULT NULL,
+  `time_in_doc` int(11) DEFAULT NULL,
+  `country` text,
+  `did_by` text,
+  `obj_name` text,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=68 DEFAULT CHARSET=utf8;
+
+
+
+
+DROP TABLE tbl_documentary_recieve;
+
+CREATE TABLE `tbl_documentary_recieve` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `number` varchar(255) DEFAULT NULL,
+  `shortcut` text,
+  `abstract` text,
+  `signed_user` varchar(255) DEFAULT NULL,
+  `keywords` text,
+  `status` int(11) DEFAULT NULL,
+  `date_recieve` int(11) DEFAULT NULL,
+  `number_doc_finish` varchar(255) DEFAULT NULL,
+  `number_doc_send` varchar(255) DEFAULT NULL,
+  `reciever` varchar(255) DEFAULT NULL,
+  `time_in_doc` int(11) DEFAULT NULL,
+  `country` text,
+  `did_by` text,
+  `obj_name` text,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=69 DEFAULT CHARSET=utf8;
+
+
+
+
+DROP TABLE tbl_guide;
+
+CREATE TABLE `tbl_guide` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `number` varchar(255) DEFAULT NULL,
+  `shortcut` text,
+  `abstract` text,
+  `signed_user` varchar(255) DEFAULT NULL,
+  `keywords` text,
+  `status` int(11) DEFAULT NULL,
+  `date_recieve` int(11) DEFAULT NULL,
+  `number_doc_finish` varchar(255) DEFAULT NULL,
+  `number_doc_send` varchar(255) DEFAULT NULL,
+  `reciever` varchar(255) DEFAULT NULL,
+  `time_in_doc` int(11) DEFAULT NULL,
+  `country` text,
+  `did_by` text,
+  `obj_name` text,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=68 DEFAULT CHARSET=utf8;
+
+
+
+
+DROP TABLE tbl_letters;
+
+CREATE TABLE `tbl_letters` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `letter_created` text,
+  `signed_date` int(11) DEFAULT NULL,
+  `signed_recieve` int(11) DEFAULT NULL,
+  `decided_assigned` text,
+  `verification_period` text,
+  `content` text,
+  `staff_assigned` text,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+
+
+
+DROP TABLE tbl_opinion;
+
+CREATE TABLE `tbl_opinion` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `number` varchar(255) DEFAULT NULL,
+  `shortcut` text,
+  `abstract` text,
+  `signed_user` varchar(255) DEFAULT NULL,
+  `keywords` text,
+  `status` int(11) DEFAULT NULL,
+  `date_recieve` int(11) DEFAULT NULL,
+  `number_doc_finish` varchar(255) DEFAULT NULL,
+  `number_doc_send` varchar(255) DEFAULT NULL,
+  `reciever` varchar(255) DEFAULT NULL,
+  `time_in_doc` int(11) DEFAULT NULL,
+  `country` text,
+  `did_by` text,
+  `obj_name` text,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=68 DEFAULT CHARSET=utf8;
+
+
+
+
+DROP TABLE tbl_wanted_abroad;
+
+CREATE TABLE `tbl_wanted_abroad` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nation_request` text,
+  `object_name` text,
+  `wanted_number` text,
+  `crime` text,
+  `date_publish` int(11) DEFAULT NULL,
+  `staff_assigned` text,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
+
+
+
+
